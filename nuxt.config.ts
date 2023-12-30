41352ad1761c536d6b22aaa17dc2af05d1002422
mio <knightopress@gmail.com>
2023-12-29 23:34:25 -0500

    Clean up Nuxt template.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  devServer: {
+    host: '127.0.0.1'
+  }
 })
