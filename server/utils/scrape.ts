0ebb9606e0c7b0158db44d8cb4fefdd65ecd1315
mio <knightopress@gmail.com>
2024-01-11 01:35:46 -0500

    Optimize jsonata code.

--- server/utils/scrape.ts
+++ server/utils/scrape.ts
@@ -41,49 +41,49 @@ export type ClassStatsFields = {
 
 export type BrStats = OverallStatsFields & ClassStatsFields
 
-function scrapeClassStats(brStatsItems: any, className: string) {
-  const classStatsQuery = jsonata(`$[customName.text = '${className} Stats']{
-    'wins': $number(customLore[text = 'Wins: '].extra.text),
-    'losses': $number(customLore[text = 'Losses: '].extra.text),
-    'kills': $number(customLore[text = 'Kills: '].extra.text),
-    'deaths': $number(customLore[text = 'Deaths: '].extra.text),
-    'assists': $number(customLore[text = 'Assists: '].extra.text),
-    'playtime': customLore[text = 'Playtime: '].extra.text,
-    'winLoseRatio': $number(customLore[text = 'W/L Ratio: '].extra.text),
-    'killDeathRatio': $number(customLore[text = 'K/D Ratio: '].extra.text)
-  }`)
+const classStatsQuery = jsonata(`$[customName.text = $classNameText]{
+  'wins': $number(customLore[text = 'Wins: '].extra.text),
+  'losses': $number(customLore[text = 'Losses: '].extra.text),
+  'kills': $number(customLore[text = 'Kills: '].extra.text),
+  'deaths': $number(customLore[text = 'Deaths: '].extra.text),
+  'assists': $number(customLore[text = 'Assists: '].extra.text),
+  'playtime': customLore[text = 'Playtime: '].extra.text,
+  'winLoseRatio': $number(customLore[text = 'W/L Ratio: '].extra.text),
+  'killDeathRatio': $number(customLore[text = 'K/D Ratio: '].extra.text)
+}`)
 
-  return classStatsQuery.evaluate(brStatsItems) as Promise<ClassStats>
+const overallStatsQuery = jsonata(`$[customName.text = 'Overall Stats']{
+  'total': {
+    'games': $number(customLore[text = 'Games Played: '].extra.text),
+    'wins': $number(customLore[text = 'Total Wins: '].extra.text),
+    'kills': $number(customLore[text = 'Total Kills: '].extra.text),
+    'assists': $number(customLore[text = 'Total Assists: '].extra.text),
+    'playtime': customLore[text = 'Total Playtime: '].extra.text
+  },
+  'solo': {
+    'games': $number(customLore[text = 'Solo Games Played: '].extra.text),
+    'wins': $number(customLore[text = 'Solo Wins: '].extra.text),
+    'kills': $number(customLore[text = 'Solo Kills: '].extra.text),
+    'assists': $number(customLore[text = 'Solo Assists: '].extra.text),
+    'playtime': customLore[text = 'Solo Playtime: '].extra.text
+  },
+  'civ': {
+    'games': $number(customLore[text = 'Civ Games Played: '].extra.text),
+    'wins': $number(customLore[text = 'Civ Wins: '].extra.text),
+    'kills': $number(customLore[text = 'Civ Kills: '].extra.text),
+    'assists': $number(customLore[text = 'Civ Assists: '].extra.text),
+    'playtime': customLore[text = 'Civ Playtime: '].extra.text
+  },
+  'legendaryCrafts': $number(customLore[text = 'Legendary Weapons Crafted: '].extra.text),
+  'legendaryKills': $number(customLore[text = 'Legendary Weapon Kills: '].extra.text),
+  'avgTimeSurvived': customLore[text = 'Average Time Survived: '].extra.text
+}`)
+
+function scrapeClassStats(brStatsItems: any, className: string) {
+  return classStatsQuery.evaluate(brStatsItems, { classNameText: `${className} Stats` }) as Promise<ClassStats>
 }
 
 async function scrapeBrStats(brStatsItems: any): Promise<BrStats> {
-  const overallStatsQuery = jsonata(`$[customName.text = 'Overall Stats']{
-    'total': {
-      'games': $number(customLore[text = 'Games Played: '].extra.text),
-      'wins': $number(customLore[text = 'Total Wins: '].extra.text),
-      'kills': $number(customLore[text = 'Total Kills: '].extra.text),
-      'assists': $number(customLore[text = 'Total Assists: '].extra.text),
-      'playtime': customLore[text = 'Total Playtime: '].extra.text
-    },
-    'solo': {
-      'games': $number(customLore[text = 'Solo Games Played: '].extra.text),
-      'wins': $number(customLore[text = 'Solo Wins: '].extra.text),
-      'kills': $number(customLore[text = 'Solo Kills: '].extra.text),
-      'assists': $number(customLore[text = 'Solo Assists: '].extra.text),
-      'playtime': customLore[text = 'Solo Playtime: '].extra.text
-    },
-    'civ': {
-      'games': $number(customLore[text = 'Civ Games Played: '].extra.text),
-      'wins': $number(customLore[text = 'Civ Wins: '].extra.text),
-      'kills': $number(customLore[text = 'Civ Kills: '].extra.text),
-      'assists': $number(customLore[text = 'Civ Assists: '].extra.text),
-      'playtime': customLore[text = 'Civ Playtime: '].extra.text
-    },
-    'legendaryCrafts': $number(customLore[text = 'Legendary Weapons Crafted: '].extra.text),
-    'legendaryKills': $number(customLore[text = 'Legendary Weapon Kills: '].extra.text),
-    'avgTimeSurvived': customLore[text = 'Average Time Survived: '].extra.text
-  }`)
-
   const overallStats = await overallStatsQuery.evaluate(brStatsItems) as OverallStatsFields
 
   const classStats: ClassStatsFields = {
