cc76be2e1f58ad5b2cfde6db86f281fd9846385b
mio <knightopress@gmail.com>
2024-01-12 12:47:21 -0500

    Fix leak test route.

--- server/api/leak-test.ts
+++ server/api/leak-test.ts
@@ -8,13 +8,26 @@ export default defineEventHandler(async () => {
     username: runtimeCfg.bot.authName,
     host: runtimeCfg.bot.serverHost,
     auth: 'microsoft',
-    profilesFolder: runtimeCfg.bot.profilesDir,
+    profilesFolder: runtimeCfg.bot.profilesDir || undefined,
   })
 
+  bot.on('resourcePack', () => {
+    console.log('Resource Pack: Accepting')
+    bot.acceptResourcePack()
+  })
+
+  bot.on('kicked', console.log)
+  bot.on('error', console.log)
+
   await once(bot, 'spawn')
+  console.log('Joined Queue')
+
   await once(bot, 'spawn')
+  console.log('Joined Lobby')
 
   bot.chat('/stats Yuishikawa')
+
+  bot.removeAllListeners()
 
   bot.quit()
 
