d97540655bd61da7a7a5f6e830ba2c6735ba158f
mio <knightopress@gmail.com>
2024-01-13 13:20:15 -0500

    Fix cache key mismatch.

--- server/utils/bot-auth-cache.ts
+++ server/utils/bot-auth-cache.ts
@@ -8,7 +8,7 @@ with Redis as the backing storage.
 
 export function createAuthCache({ cacheName, username }: { cacheName: string, username: string }) {
   const authCache = {
-    cacheKey: `bot-session:${username}:${cacheName}`,
+    cacheKey: `bot-session:${cacheName}:${username}`,
     cacheStore: useStorage('cache'),
     cached: null as any,
     async loadInitialValue() {
