1465b27cd8a21ac10a3aa7564a864ad3f60914fe
mio <knightopress@gmail.com>
2024-01-13 22:50:01 -0500

    Make background color consistent across browsers.

--- app.vue
+++ app.vue
@@ -11,4 +11,10 @@
   @apply grid min-h-screen;
   grid-template-rows: min-content auto min-content;
 }
+
+@media (prefers-color-scheme: dark) {
+  #__nuxt {
+    background-color: color-mix(in srgb, rgb(var(--color-gray-900)), #000 25%);
+  }
+}
 </style>
