735dd212899d24e632dacf7f044e55d479e0117e
mio <knightopress@gmail.com>
2024-03-15 13:32:34 -0400

    Switch to `msal` auth (`live` auth appears to be unreliable).

--- server/utils/mineflayer.ts
+++ server/utils/mineflayer.ts
@@ -19,6 +19,11 @@ const createNewBot = async () => {
     port: runtimeCfg.bot.serverPort,
     version: '1.20.1',
     checkTimeoutInterval: 60 * 1000,
+    // https://github.com/PrismarineJS/node-minecraft-protocol/blob/master/src/client/microsoftAuth.js#L11
+    // https://github.com/PrismarineJS/prismarine-auth/blob/master/src/common/Constants.js
+    authTitle: '389b1b32-b5d5-43b2-bddc-84ce938d6737',
+    // @ts-ignore
+    flow: 'msal',
   })
 
   const onResourcePack = () => {
