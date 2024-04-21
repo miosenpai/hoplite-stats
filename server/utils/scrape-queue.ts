015c456553c4a6915d3c71180b79118ce70ea70c
mio <knightopress@gmail.com>
2024-04-21 12:25:42 -0400

    Add overall task timeout.

--- server/utils/scrape-queue.ts
+++ server/utils/scrape-queue.ts
@@ -1,6 +1,9 @@
 import PQueue from 'p-queue'
 
-const scrapeQueue = new PQueue({ concurrency: 1 })
+const scrapeQueue = new PQueue({
+  concurrency: 1,
+  timeout: 7500,
+})
 
 export const useScrapeQueue = () => {
   return scrapeQueue
